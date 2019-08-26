624790528b2d4549776d811dbf4e5a39b65573cb
Ryan Moore <rymoore411@gmail.com>
2019-08-25 18:15:13 -0700

    added some stylin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {HashRouter, Route } from 'react-router-dom';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
+import { MDBAnimation} from 'mdbreact';
 
 export default class App extends Component {
   constructor(){
@@ -10,6 +11,7 @@ export default class App extends Component {
       image: 'https://cdn.pixabay.com/photo/2013/07/12/12/55/black-widow-146550_1280.png',
       friend: null,
       name: '',
+      spinner: false,
     }
     this.handleDrop = this.handleDrop.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -18,9 +20,12 @@ export default class App extends Component {
   handleClick(){
     event.preventDefault();
     this.setState({friend: null});
+    this.setState({spinner: false});
   }
 
   async handleDrop(file){
+    this.setState({spinner: true});
+
     const data = new FormData()
     data.append('file', file[0]);
     const response = await axios.post('/spider', data);
@@ -30,12 +35,12 @@ export default class App extends Component {
   }
 
   render() {
-    const {image, friend} = this.state;
+    const {image, friend, spinner} = this.state;
     const {handleDrop, handleClick} = this;
 
     if(friend === null){
       return (
-        <div>
+        <div className="container-fluid">
           <h2>Spider Bro App</h2>
           <Dropzone onDrop={handleDrop}>
           {({getRootProps, getInputProps}) => (
@@ -43,7 +48,9 @@ export default class App extends Component {
               <div {...getRootProps()}>
                 <input {...getInputProps()}/>
                   <p>Drag spider pics on spider bro</p>
-                  <img src={image} style={{ width: '20%', height: 250 }}/>
+                  <MDBAnimation type={spinner === false ? 'bounce' : 'flash'} infinite>
+                  <img src={image} className="img-fluid" style={{maxWidth: '10%'}}/>
+                  </MDBAnimation>
               </div>
             </section>
           )}
@@ -53,20 +60,48 @@ export default class App extends Component {
     }
     else if(friend === true){
       return(
-        <div>
-          <h2>He's Chill</h2>
-          <img src="https://cdn.pixabay.com/photo/2014/04/02/16/29/spider-307448_1280.png" style={{ width: '20%', height: 250 }}/>
-          <button onClick={handleClick}>Find More Spider bros</button>
+        <div className="container-fluid" style={{display: 'flex', maxWidth: '40%', alignContent: 'center', justifyContent: 'center'}}>
+          <div className="card">
+            <div className="view overlay">
+              <img className="card-img-top" src="https://cdn.pixabay.com/photo/2014/04/02/16/29/spider-307448_1280.png" alt="Card image cap"/>
+              <a>
+                <div className="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+
+          <div className="card-body elegant-color white-text rounded-bottom">
+            <a className="activator waves-effect mr-4"><i className="fas fa-share-alt white-text"></i></a>
+            <h4 className="card-title">He's Chill</h4>
+            <hr className="hr-light"/>
+            <a href="#!" className="white-text d-flex justify-content-end"></a>
+            <a href="#" className="btn btn-primary" onClick={handleClick}>Find More Spider Bros</a>
+          </div>
         </div>
+      </div>
       )
     }
     else{
       return(
-        <div>
-          <h2>Watch out!</h2>
-          <img src="https://images.pexels.com/photos/2665108/pexels-photo-2665108.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" style={{ width: '20%', height: 250 }}/>
-          <button onClick={handleClick}>Find More Spider bros</button>
+        <div className="container-fluid" style={{display: 'flex', maxWidth: '40%', alignContent: 'center', justifyContent: 'center'}}>
+          <div className="card">
+            <div className="view overlay">
+              <img className="card-img-top" src="https://images.pexels.com/photos/2665108/pexels-photo-2665108.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="Card image cap"/>
+              <a>
+                <div className="mask rgba-white-slight"></div>
+              </a>
+            </div>
+
+
+          <div className="card-body elegant-color white-text rounded-bottom">
+            <a className="activator waves-effect mr-4"><i className="fas fa-share-alt white-text"></i></a>
+            <h4 className="card-title">Watch out!</h4>
+            <hr className="hr-light"/>
+            <a href="#!" className="white-text d-flex justify-content-end"></a>
+            <a href="#" className="btn btn-primary" onClick={handleClick}>Find More Spider Bros</a>
+          </div>
         </div>
+      </div>
       )
     }
   }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import 'bootstrap';
 
 const root = document.querySelector('#root');
 ReactDOM.render(<App />, root);
